b66ccb07a2ed44c490b2b7844c16b1f7ccdce450
Satyadev Mishra <iamsatyadevmishra@gmail.com>
2026-02-02 17:49:34 +0530

    Update admitCardData.ts

--- src/data/admitCardData.ts
+++ src/data/admitCardData.ts
@@ -23,18 +23,18 @@ export interface AdmitCardData {
 }
 
 export const admitCardData: AdmitCardData = {
-  startTime: "09:00 AM",
+  startTime: "08:30 AM",
   endTime: "11:30 AM",
   class: {
     "9": {
       subjects: [
-        { "name": "Maths", "day": "Thursday", "date": "22-01-2026" },
-        { "name": "English", "day": "Friday", "date": "23-01-2026" },
-        { "name": "Science", "day": "Saturday", "date": "24-01-2026" },
-        { "name": "Hindi", "day": "Wednesday", "date": "28-01-2026" },
-        { "name": "Physical Edu", "day": "Thursday", "date": "29-01-2026" },
-        { "name": "SKT", "day": "Friday", "date": "30-01-2026" },
-        { "name": "Social Science", "day": "Monday", "date": "02-02-2026" }
+        { "name": "Hindi", "day": "Tuesday", "date": "17-02-2026" },
+        { "name": "Social Science", "day": "Thursday", "date": "19-02-2026" },
+        { "name": "English", "day": "Saturday", "date": "21-02-2026" },
+        { "name": "Math", "day": "Monday", "date": "23-02-2026" },
+        { "name": "Science", "day": "Wednesday", "date": "25-02-2026" },
+        { "name": "Physical Edu", "day": "Friday", "date": "27-02-2026" },
+        { "name": "SKT", "day": "Monday", "date": "02-03-2026" },
       ]
     },
     "10": {
@@ -49,21 +49,21 @@ export const admitCardData: AdmitCardData = {
     },
     "11-arts": {
       subjects: [
-        { "name": "History", "day": "Friday", "date": "23-01-2026" },
-        { "name": "English (Core/Elective)", "day": "Saturday", "date": "24-01-2026" },
-        { "name": "Hindi (Core/Elective) ", "day": "Wednesday", "date": "28-01-2026" },
-        { "name": "Political Sci.", "day": "Thursday", "date": "29-01-2026" },
-        { "name": "Physical Edu", "day": "Monday", "date": "02-02-2026" },
+        { "name": "English (Core/Elective)", "day": "Monday", "date": "16-02-2026" },
+        { "name": "Hindi (Core/Elective) ", "day": "Wednesday", "date": "18-02-2026" },
+        { "name": "Physical Edu", "day": "Tuesday", "date": "24-02-2026" },
+        { "name": "History", "day": "Friday", "date": "27-02-2026" },
+        { "name": "Political Sci.", "day": "Saturday", "date": "28-02-2026" },
       ]
     },
     "11-commerce": {
       subjects: [
-        { "name": "Economics", "day": "Friday", "date": "23-01-2026" },
-        { "name": "English (Core/Elective)", "day": "Saturday", "date": "24-01-2026" },
-        { "name": "Hindi (Core/Elective)", "day": "Wednesday", "date": "28-01-2026" },
-        { "name": "Accountancy", "day": "Friday", "date": "30-01-2026" },
-        { "name": "Business Studies", "day": "Monday", "date": "02-02-2026" },
-        { "name": "Maths", "day": "Tuesday", "date": "03-02-2026" },
+        { "name": "English (Core/Elective)", "day": "Monday", "date": "16-02-2026" },
+        { "name": "Hindi (Core/Elective) ", "day": "Wednesday", "date": "18-02-2026" },
+        { "name": "Economics", "day": "Monday", "date": "23-02-2026" },
+        { "name": "Accountancy", "day": "Wednesday", "date": "25-02-2026" },
+        { "name": "Business Studies", "day": "Friday", "date": "27-02-2026" },
+        { "name": "Maths", "day": "Monday", "date": "02-03-2026" },
       ]
     },
     "12-arts": {
